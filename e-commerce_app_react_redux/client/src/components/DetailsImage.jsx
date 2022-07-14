71174e6d420ba0e8a7ef5a25d8a5388e905c4ae8
akram-xujaniyozov <75637227+akram-xujaniyozov@users.noreply.github.com>
2022-07-14 12:47:08 +0500

    Update DetailsImage.jsx

--- e-commerce_app_react_redux/client/src/components/DetailsImage.jsx
+++ e-commerce_app_react_redux/client/src/components/DetailsImage.jsx
@@ -1,28 +1,22 @@
-import React from "react";
+import React from 'react';
 
 const DetailsImage = ({ product }) => {
+  const images = product.images?.map((img, i) => (
+    <img
+      style={{ height: '5rem', width: '5rem' }}
+      key={Number.parseInt(i)}
+      src={img}
+      alt={`Photo ${i}`}
+    />
+  ));
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-4">
       <div className="border border-light border-1">
-        <img
-          src={product.thumbnail}
-          alt="Main photo"
-          style={{ height: "25rem", width: "30rem" }}
-        />
+        <img src={product.thumbnail} alt="Main photo" style={{ height: '25rem', width: '30rem' }} />
       </div>
-      <div
-        className="mt-4 d-flex flex-row justify-content-between"
-        style={{ width: "100%" }}
-      >
-        {product.images.map((img, i) => {
-          return (
-            <img
-              style={{ height: "5rem", width: "5rem" }}
-              src={img}
-              alt={`sub photo ${i}`}
-            />
-          );
-        })}
+      <div className="mt-4 d-flex flex-row justify-content-between" style={{ width: '100%' }}>
+        {images}
       </div>
     </div>
   );
